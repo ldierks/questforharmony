c05c04aa3fce29e0f8d2a32cd160965394c463a6
Janina88 <j.torbecke@gmail.com>
2017-12-02 12:29:41 +0100

    sound in room

--- Assets/Room.cs
+++ Assets/Room.cs
@@ -6,37 +6,61 @@ using UnityEngine;
 public class Room : MonoBehaviour {
 
 	public AudioClip backgroundSound;
+	public new AudioSource audio;
+
+	bool fadein = false;
+	bool fadeout = false;
+	public float fadeInOutSpeed;
 	//public bool preloadAudioData;
 
 	// Use this for initialization
 	void Start () {
-
+		this.audio = this.gameObject.GetComponent<AudioSource>();
+		this.audio.clip = this.backgroundSound;
+		this.audio.volume = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (this.fadein) {
+			Debug.Log("Increase volume");
+			this.audio.volume = Mathf.Min(this.audio.volume + this.fadeInOutSpeed, 1.0f);
+		}
+		if (this.fadein == true && this.audio.volume == 1.0f) {
+			Debug.Log("fadein stop");
+			this.fadein = false;
+		}
+
+		if (this.fadeout) {
+			this.audio.volume = Mathf.Max(this.audio.volume - this.fadeInOutSpeed, 0.0f);
+			Debug.Log("Decrease stop");
+		}
+		if (this.fadeout == true && this.audio.volume == 0.0f) {
+			Debug.Log("fadeout stop");
+			this.fadeout = false;
+			this.audio.Stop();
+		}
 	}
 
 	void OnTriggerEnter(Collider collider) {
 		Debug.Log("Enter " + this.gameObject.name + " " + collider.name);
 
-		this.gameObject.GetComponent<AudioSource>().clip = this.backgroundSound;
+		this.PlaySound();
+		this.fadeout = false;
+		this.fadein = true;
 	}
 
 	void OnTriggerExit(Collider collider) {
 		Debug.Log("Exit " + this.gameObject.name + " " + collider.name);
+
+		this.fadein = false;
+		this.fadeout = true;
 	}
 
 
-	IEnumerator PlaySound() {
+	void PlaySound() {
 		Debug.Log("Play sound");
 
-		AudioSource audio = GetComponent<AudioSource>();
-
-		audio.Play();
-		yield return new WaitForSeconds(audio.clip.length);
-		audio.clip = this.backgroundSound;
 		audio.Play();
 	}
 
